96ead53e4f5c95820fa6e9211fcadcda00bc7b9e
eko.susilo@paytren.co.id <eko.susilo@paytren.co.id>
2023-07-24 14:03:27 +0700

    seeder

--- resources/views/content/invoice.blade.php
+++ resources/views/content/invoice.blade.php
@@ -31,47 +31,7 @@
                 <!-- /.col -->
               </div>
               <!-- info row -->
-              <div class="card card-info">
-                <div class="card-header">
-                  <h3 class="card-title">Alamat Pengirim</h3>
-                </div>
-                <form action="{{ route('produk.simpan.alamat') }}" method="POST">
-                  @csrf
-                  <div class="card-body">
-                    <div class="input-group input-group-sm mb-3">
-                      <input type="text" class="form-control" placeholder="Alamat" name="alamat">
-                    </div>
-                    <div class="input-group input-group-sm mb-3">
-                      <select class="form-control select2 placeholder-provinsi" style="width: 100%;" name="provinsi">
-                        <option selected="selected"></option>
-                        @forelse ($prov as $p)    
-                          <option value="{{ $p->province_id }}">{{ $p->province_name }}</option>
-                        @empty
-                        @endforelse
-                      </select>
-                    </div>
-                    <div class="input-group input-group-sm mb-3">
-                      <select class="form-control select2 placeholder-kota" style="width: 100%;" name="kota">
-                        <option selected="selected"></option>
-                      </select>
-                    </div>
-                    <div class="input-group input-group-sm mb-3">
-                      <select class="form-control select2 placeholder-kecamatan" style="width: 100%;" name="kecamatan">
-                        <option selected="selected"></option>
-                      </select>
-                    </div>
-                    <div class="input-group input-group-sm mb-3">
-                      <input type="text" class="form-control" placeholder="No Telp" name="telp">
-                    </div>
-                    <div class="input-group input-group-sm mb-3">
-                      <span class="input-group-append">
-                        <button type="submit" class="btn btn-info btn-flat">Submit</button>
-                      </span>
-                    </div>
-                    <!-- /input-group -->
-                  </div>
-                </form>
-              </div>
+              
               @if(!is_null($cek_user))
                 <div class="row invoice-info">
                   <div class="col-sm-4 invoice-col">
@@ -87,6 +47,66 @@
                   </div>
                 </div>
               @endif
+
+              <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
+                Tambah Alamat Pengiriman
+              </button>
+
+              <!-- Modal -->
+              <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div class="modal-dialog" role="document">
+                  <div class="modal-content">
+                    <div class="modal-header">
+                      <h5 class="modal-title" id="exampleModalLabel">Alamat Pengirim</h5>
+                      <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                      </button>
+                    </div>
+                    <div class="modal-body">
+                      <form action="{{ route('produk.simpan.alamat') }}" method="POST">
+                        @csrf
+                        <div class="card-body">
+                          <div class="input-group input-group-sm mb-3">
+                            <input type="text" class="form-control" placeholder="Alamat" name="alamat">
+                          </div>
+                          <div class="input-group input-group-sm mb-3">
+                            <select class="form-control select2 placeholder-provinsi" style="width: 100%;" name="provinsi">
+                              <option selected="selected"></option>
+                              @forelse ($prov as $p)    
+                                <option value="{{ $p->province_id }}">{{ $p->province_name }}</option>
+                              @empty
+                              @endforelse
+                            </select>
+                          </div>
+                          <div class="input-group input-group-sm mb-3">
+                            <select class="form-control select2 placeholder-kota" style="width: 100%;" name="kota">
+                              <option selected="selected"></option>
+                            </select>
+                          </div>
+                          <div class="input-group input-group-sm mb-3">
+                            <select class="form-control select2 placeholder-kecamatan" style="width: 100%;" name="kecamatan">
+                              <option selected="selected"></option>
+                            </select>
+                          </div>
+                          <div class="input-group input-group-sm mb-3">
+                            <input type="text" class="form-control" placeholder="No Telp" name="telp">
+                          </div>
+                          <div class="input-group input-group-sm mb-3">
+                            <span class="input-group-append">
+                              <button type="submit" class="btn btn-info btn-flat">Submit</button>
+                            </span>
+                          </div>
+                          <!-- /input-group -->
+                        </div>
+                      </form>
+                    </div>
+                    <div class="modal-footer">
+                      <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                      <button type="button" class="btn btn-primary">Save changes</button>
+                    </div>
+                  </div>
+                </div>
+              </div>
               <!-- /.row -->
 
               <!-- Table row -->
@@ -129,64 +149,34 @@
                 <!-- /.col -->
               </div>
               <!-- /.row -->
-              <div class="small-12 cell">
-        <div class="icard with-shadow data-card">
-          <div class="icard-body">
-            <div class="checkout grid-x grid-margin-x">
-              <div class="large-8 large-offset-2 cell">
-                <div class="card-title tal mb10">Metode Bayar</div>
-                <div class="payment_methods_vo mb0">
-                  <!-- <div class="payment_opt">
-                    <label for="pay_paytren">
-                      <input type="radio" id="pay_paytren" name="payment_method" value="EMONEY" {{ $sel->payment_method=='EMONEY' ? 'checked' : '' }} {{ ($sel->payment_method && $sel->payment_method!='EMONEY') ? 'disabled' : '' }}> <strong>Paytren eMoney</strong>
-                      <div class="info-block">
-                        <p>Pastikan sudah download dan install Aplikasi Paytren 5.17, jika belum <a href="https://play.google.com/store/apps/details?id=id.co.paytren.user" target="_blank">klik disini</a> untuk Android, <a href="https://apps.apple.com/id/app/paytren-5-17/id1387123517" target="_blank">klik disini</a> untuk iOS</p>
-                      </div>
-                    </label>
-                  </div>-->
-                  <div class="payment_opt">
-                    <label for="pay_bank_Mandiri">
-                      <input type="radio" id="pay_bank_Mandiri" name="payment_method" value="MDR" {{ $sel->payment_method=='MDR' ? 'checked' : '' }} {{ ($sel->payment_method && $sel->payment_method!='MDR') ? 'disabled' : '' }}> <strong>Mandiri (Transfer Bank)</strong>
-                      <div class="info-block">
-                        <p>Melalui ATM, Mobile Banking atau Internet Banking</p>
-                      </div>
-                    </label>
-                  </div>
-                  <div class="payment_opt">
-                    <label for="pay_bank_BCA">
-                      <input type="radio" id="pay_bank_BCA" name="payment_method" value="RINTIS_BCA" {{ $sel->payment_method=='RINTIS_BCA' ? 'checked' : '' }} {{ ($sel->payment_method && $sel->payment_method!='RINTIS_BCA') ? 'disabled' : '' }}> <strong>BCA (Transfer Bank)</strong>
-                      <div class="info-block">
-                        <p>Melalui ATM, Mobile Banking atau Internet Banking</p>
-                      </div>
-                    </label>
-                  </div>
-                                    <div class="payment_opt">
-                    <label for="pay_narobil">
-                      <input type="radio" id="pay_narobil" name="payment_method" value="NAROBIL" {{ $sel->payment_method=='NAROBIL' ? 'checked' : '' }} {{ ($sel->payment_method && $sel->payment_method!='NAROBIL') ? 'disabled' : '' }}> <strong>Narobil</strong>
-                      <div class="info-block">
-                        <p>Melalui Indomaret/Alfamart/Alfamidi</p>
-                      </div>
-                    </label>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
               <div class="row">
                 <!-- accepted payments column -->
                 <div class="col-6">
-                  <p class="lead">Payment Methods:</p>
+                  <p class="lead">Metode Bayar</p>
                   <img src="../../dist/img/credit/visa.png" alt="Visa">
                   <img src="../../dist/img/credit/mastercard.png" alt="Mastercard">
                   <img src="../../dist/img/credit/american-express.png" alt="American Express">
                   <img src="../../dist/img/credit/paypal2.png" alt="Paypal">
 
                   <p class="text-muted well well-sm shadow-none" style="margin-top: 10px;">
-                    Etsy doostang zoodles disqus groupon greplin oooj voxy zoodles, weebly ning heekya handango imeem
-                    plugg
-                    dopplr jibjab, movity jajah plickers sifteo edmodo ifttt zimbra.
+                    <label for="pay_bank_Mandiri">
+                      <input type="radio" id="pay_bank_Mandiri" name="payment_method" value="MDR"> <strong>Mandiri (Transfer Bank)</strong>
+                      <div class="info-block">
+                        <p>Melalui ATM, Mobile Banking atau Internet Banking</p>
+                      </div>
+                    </label>
+                    <label for="pay_bank_BCA">
+                      <input type="radio" id="pay_bank_BCA" name="payment_method" value="RINTIS_BCA" > <strong>BCA (Transfer Bank)</strong>
+                      <div class="info-block">
+                        <p>Melalui ATM, Mobile Banking atau Internet Banking</p>
+                      </div>
+                    </label>
+                    <label for="pay_narobil">
+                      <input type="radio" id="pay_narobil" name="payment_method" value="NAROBIL"> <strong>Narobil</strong>
+                      <div class="info-block">
+                        <p>Melalui Indomaret/Alfamart/Alfamidi</p>
+                      </div>
+                    </label>
                   </p>
                 </div>
                 <!-- /.col -->
